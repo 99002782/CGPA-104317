fbe0b619a4b0984fb0624bd0ed671ea444d054ed
Seru Manasa <40772201+stepin123456104317@users.noreply.github.com>
2020-10-07 10:24:01 +0530

    Update function.c

--- function.c
+++ function.c
@@ -1,41 +1,4 @@
-#include <stdio.h>
-void calculate(int cgpa);
-int main()
 
-{
-
-int num_sem = 0;
-
-float temp = 0;
-
-float sum = 0;
-
-float gpa = 0.0;
-
-printf("Enter number of semesters\n");
-
-scanf("%d", &num_sem);
-
-printf("Enter max GPA per semester Example: In US it is 4, in India it is 10\n");
-int i;
-
-for(i =1; i<= num_sem; i++)
-
-{
-
-printf("Enter %d sem GPA\t", i);
-
-scanf("%f", &temp);
-
-sum += temp;
-
-}
-
-gpa = sum/(float)num_sem;
-
-printf("Over all GPA is\t%f\n", gpa );
-calculate(gpa);
-}
 void calculate(int cgpa)
 {
 
